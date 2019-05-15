7df8ded0aaaf4cc77a9709c5ee774a2bd2e48068
Mohit Jain <reachoutmohit@gmail.com>
2019-05-15 09:02:50 +0530

    train method returns history object

--- DeepNER/main.py
+++ DeepNER/main.py
@@ -9,15 +9,18 @@ Created on Thu May 09 09:45:20 2019
 import os
 import json
 
-from config import MODELS_PATH,ROOT_PATH,WORD_DICT_PATH,LABEL_DICT_PATH,EMBEDDING_DIM,ACTIVATION_FUNCTION,VALIDATION_SPLIT,NUM_EPOCHS,BATCH_SIZE,DROP_RATE_DENSE,DROP_RATE_LSTM,NUM_LSTM_UNITS,MAX_SEQUENCE_LENGTH
+from config import MODELS_PATH,PLOT_IMG_DIR_ROOT,ROOT_PATH,WORD_DICT_PATH,LABEL_DICT_PATH,EMBEDDING_DIM,ACTIVATION_FUNCTION,VALIDATION_SPLIT,NUM_EPOCHS,BATCH_SIZE,DROP_RATE_DENSE,DROP_RATE_LSTM,NUM_LSTM_UNITS,MAX_SEQUENCE_LENGTH
 from model import DeepNER
 from data_prep import Dataset,get_train_test,get_sent_token_label
-
+from utils import generate_hist_plot
 
 if __name__ == '__main__':
 
 	if not os.path.exists(MODELS_PATH):
 		os.makedirs(MODELS_PATH)
+
+	if not os.path.exists(PLOT_IMG_DIR_ROOT):
+		os.makedirs(PLOT_IMG_DIR_ROOT)
 
 	TRAIN_PATH = os.path.join(ROOT_PATH,'train.txt')
 	TEST_PATH = os.path.join(ROOT_PATH,'test.txt')
@@ -38,10 +41,19 @@ if __name__ == '__main__':
 
 	X_train, X_test, y_train, y_test = get_train_test(wrd2idx,label2idx,sentences,labels)
 		
-	model = DeepNER(EMBEDDING_DIM,ACTIVATION_FUNCTION,VALIDATION_SPLIT,NUM_EPOCHS,BATCH_SIZE,DROP_RATE_DENSE,DROP_RATE_LSTM,NUM_LSTM_UNITS)
+	model = DeepNER(EMBEDDING_DIM,\
+			   ACTIVATION_FUNCTION,\
+			   VALIDATION_SPLIT,\
+			   NUM_EPOCHS,\
+			   BATCH_SIZE,\
+			   DROP_RATE_DENSE,\
+			   DROP_RATE_LSTM,\
+			   NUM_LSTM_UNITS)
 	
-	model_fitted, bst_model_path = model.train_model(X_train,\
-													y_train,\
-													labels,\
-													word_index=wrd2idx,\
-													MAX_SEQUENCE_LENGTH=MAX_SEQUENCE_LENGTH)
+	history, model, bst_model_path = model.train_model(X=X_train,
+												y=y_train,\
+												labels=labels,\
+												word_index=wrd2idx,\
+												MAX_SEQUENCE_LENGTH=MAX_SEQUENCE_LENGTH)
+
+	generate_hist_plot(history)
